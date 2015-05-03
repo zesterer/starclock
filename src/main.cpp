f6ca11ec8a8087232f60836bbac0724965d0e830
zesterer <barry.of.smith@gmail.com>
2015-05-03 14:00:52 +0100

    Set up cmake and compilation structure

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//----STANDARD----
+#include "stdio.h"
+
+//----LOCAL----
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
